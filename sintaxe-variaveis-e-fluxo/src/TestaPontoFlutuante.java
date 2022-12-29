dc5382ff156e57a0b1e620337a118642424198c0
Ruan Narici <ruannarici@hotmail.com>
2022-12-28 21:41:10 -0300

    :heavy_check_mark: Java-OOP

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,15 @@
+
+public class TestaPontoFlutuante {
+	public static void main (String[] args) {
+		double salario = 1250.70;
+		int divisaoInt = 5 / 2;
+		double divisaoDouble = 5 / 2;
+		double divisaoDoubleCorrect = 5.0 / 2.0;
+		
+		System.out.println("Meu salário é: " + salario);
+		System.out.println("** 5 / 2 **");
+		System.out.println("Divisão de inteiro: " + divisaoInt);
+		System.out.println("Divisão de double inteiro: " + divisaoDouble);
+		System.out.println("Divisão de double decimal: " + divisaoDoubleCorrect);
+	}
+}
