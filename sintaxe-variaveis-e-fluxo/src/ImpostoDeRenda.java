f99a039ee1cad636bdff68aca376b33a695a9650
Ruan Narici <ruannarici@hotmail.com>
2022-12-29 14:19:07 -0300

    :heavy_check_mark: Java-OOP

---
+++ sintaxe-variaveis-e-fluxo/src/ImpostoDeRenda.java
@@ -0,0 +1,26 @@
+
+public class ImpostoDeRenda {
+	
+	public static void main(String[] args) {
+		double salario = 3000;
+		
+		if (salario >= 1900 && 
+			salario <= 2800) {
+			System.out.println("O IR é de 7.5%.");
+			System.out.println("O valor da declaração será de R$ 142,00.");
+		}
+		else if (salario >= 2800.01 && 
+				 salario <= 3751) {
+			System.out.println("O IR é de 15%.");
+			System.out.println("O valor da declaração será de R$ 350,00.");
+		}
+		else if (salario >= 3751.01 && 
+				 salario <= 4664) {
+			System.out.println("O IR é de 22,5%.");
+			System.out.println("O valor da declaração será de R$ 636,00.");
+		}
+		else {
+			System.out.println("Não será necessário declarar o Imposto de Renda (IR).");
+		}
+	}
+}
