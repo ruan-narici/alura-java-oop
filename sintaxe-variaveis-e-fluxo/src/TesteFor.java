dc51aaba7523e8521ce8bd84cddfc66d4ea1f161
Ruan Narici <ruannarici@hotmail.com>
2022-12-29 14:58:48 -0300

    :heavy_check_mark: Java-OOP

---
+++ sintaxe-variaveis-e-fluxo/src/TesteFor.java
@@ -0,0 +1,10 @@
+
+public class TesteFor {
+
+	public static void main(String[] args) {
+		for (int contador = 0; contador <= 10; contador++) {
+			System.out.println(contador);
+		}
+		// Após o bloco for fechar, a variável contador é finalizada.
+	}
+}
