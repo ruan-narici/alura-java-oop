32e90aa5f75b2391c03ff727a6896f0f05e5b2d9
Ruan Narici <ruannarici@hotmail.com>
2022-12-28 21:13:55 -0300

    :heavy_check_mark: Java-OOP

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,13 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		int idadeFilho;
+		int idadeMae;
+		
+		idadeFilho = 24;
+		System.out.println("A idade de Ruan é: " + idadeFilho + " anos.");
+		idadeMae = idadeFilho + 20;
+		System.out.println("A idade da Mãe de Ruan é: "+ idadeMae + " anos.");
+		System.out.println("Sendo assim, a Mãe de Ruan é 20 anos mais velha do que ele.");
+	}
+}
