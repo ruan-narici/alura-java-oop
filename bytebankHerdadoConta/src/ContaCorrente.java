b1551359ce9cff953d67d61babefc88b6cd71593
Ruan Narici <ruannarici@hotmail.com>
2023-01-10 20:23:54 -0300

    :heavy_check_mark: Java-OOP

--- bytebankHerdadoConta/src/ContaCorrente.java
+++ bytebankHerdadoConta/src/ContaCorrente.java
@@ -1,5 +1,5 @@
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 
 	public ContaCorrente(int numero, int agencia) {
 		super(numero, agencia);
@@ -16,4 +16,10 @@ public class ContaCorrente extends Conta {
 		super.saldo += valor;
 		
 	}
+
+	@Override
+	public double getValorImposto() {
+		return super.saldo * 0.01;
+	}
+	
 }
