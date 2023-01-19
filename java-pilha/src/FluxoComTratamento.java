7a907c3d22df61e9f1a3efaaf3d9fbd8a9b81a72
Ruan Narici <ruannarici@hotmail.com>
2023-01-19 11:19:10 -0300

    :heavy_check_mark: Java-OOP

---
+++ java-pilha/src/FluxoComTratamento.java
@@ -0,0 +1,30 @@
+
+public class FluxoComTratamento {
+
+	public static void main(String[] args) {
+		System.out.println("Iniciando o fluxo...");
+		try {
+		metodo1();
+		} catch (ArithmeticException ex) {
+			String msg = ex.getMessage();
+			System.out.println("Erro: " + msg);
+			ex.printStackTrace();
+		}
+		System.out.println("Finalizando o main.");
+	}
+	
+	private static void metodo1() {
+		System.out.println("Iniciando o metodo 1...");
+		metodo2();
+		System.out.println("Finalizando o metodo 1.");
+	}
+	
+	private static void metodo2() {
+		System.out.println("Iniciando o metodo 2...");
+//		ArithmeticException exception = new ArithmeticException("Metodo 2 Error");
+//		throw exception;
+		throw new ArithmeticException("Metodo 2 Error");
+		
+	}
+	
+}
