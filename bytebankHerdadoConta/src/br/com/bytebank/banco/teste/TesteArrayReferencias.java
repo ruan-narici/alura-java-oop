5f200b23354994d688594b2f6833d7e540dc767f
Ruan Narici <ruannarici@hotmail.com>
2023-01-30 12:02:46 -0300

    :heavy_check_mark: Java-OOP

--- bytebankHerdadoConta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ bytebankHerdadoConta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,20 +1,25 @@
 package br.com.bytebank.banco.teste;
 
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
 		ContaCorrente cc1 = new ContaCorrente(222, 113);
-		ContaCorrente cc2 = new ContaCorrente(333, 123);
+		ContaPoupanca cc2 = new ContaPoupanca(333, 123);
 		
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Conta[] contas = new Conta[5];
 		
 		contas[0] = cc1;
 		contas[1] = cc2;
+		ContaCorrente ref = (ContaCorrente) contas[0];
+	
 		
 		System.out.println("Agencia: " + contas[0].getAgencia() + ", Numero: " + contas[0].getNumero());
 		System.out.println("Agencia: " + contas[1].getAgencia() + ", Numero: " + contas[1].getNumero());
+		System.out.println("Agencia: " + ref.getAgencia() + ", Numero: " + ref.getNumero());
 	}
 }
