0224fca60dfc5d7468360d5ee417f5df58c24165
Ruan Narici <ruannarici@hotmail.com>
2023-01-31 20:20:38 -0300

    :heavy_check_mark: Java-OOP

--- bytebankHerdadoConta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
+++ bytebankHerdadoConta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
@@ -1,17 +1,23 @@
 package br.com.bytebank.banco.teste.util;
 import java.util.ArrayList;
-import br.com.bytebank.banco.modelo.*;
+
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
 public class TesteArrayList {
 	
 	public static void main(String[] args) {
 		
-		ArrayList lista = new ArrayList();
+		ArrayList<Conta> lista = new ArrayList<Conta>();
 		
 		Conta cc1 = new ContaCorrente(213,452);
 		Conta cc2 = new ContaCorrente(613,122);
+		Cliente cl1 = new Cliente();
 	
 		lista.add(cc1);
 		lista.add(cc2);
+		//Erro ocasionado pelo fato do ARrayList so armazenar classes do tipo Conta;
+		//lista.add(cl1);
 		
 		System.out.println(lista.get(0));
 		System.out.println(lista.get(1));
