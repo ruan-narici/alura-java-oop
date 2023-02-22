2978aace52bbf1941aec7a8c6857b9e0a6e00f04
Ruan Narici <ruannarici@hotmail.com>
2023-02-22 13:48:03 -0300

    :heavy_check_mark: Java-OOP

--- java8/src/java8/TesteDefaultMethods.java
+++ java8/src/java8/TesteDefaultMethods.java
@@ -5,44 +5,41 @@ import java.util.Comparator;
 import java.util.List;
 
 public class TesteDefaultMethods {
-	
+
 	public static void main(String[] args) {
-		
+
 		List<String> palavras = new ArrayList<>();
-		Comparator<String> comparar = new Comparacao();
-		
+//		Comparator<String> comparar = new Comparacao();
+
 		palavras.add("Aula online");
 		palavras.add("Casa do código");
 		palavras.add("Java na Aula");
 		palavras.add("Caelum");
-		
+
 		palavras.forEach(palavra -> {
 			System.out.println(palavra);
 		});
-		
-		palavras.sort(comparar);
-		
+
+//		palavras.sort(comparar);
+
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
 		System.out.println();
-		palavras.forEach(palavra -> {
-			System.out.println(palavra);
-		});
+		palavras.forEach(palavra -> System.out.println(palavra));
+
 	}
-	
 }
 
-class Comparacao implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		if (s1.length() < s2.length()) {
-			return -1;
-		} else if (s1.length() > s2.length()) {
-			return 1;
-		}
-		return 0;
-	}
-
-}
-
-
-
+//class Comparacao implements Comparator<String> {
+//
+//	@Override
+//	public int compare(String s1, String s2) {
+//		if (s1.length() < s2.length()) {
+//			return -1;
+//		} else if (s1.length() > s2.length()) {
+//			return 1;
+//		}
+//		return 0;
+//	}
+//
+//}
