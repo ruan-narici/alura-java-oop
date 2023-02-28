d1ab04ad0916ed26ac1ccfde2f4726193f884aa6
Ruan Narici <ruannarici@hotmail.com>
2023-02-28 16:46:42 -0300

    :heavy_check_mark: Java-OOP

--- tddFuncionario/src/br/com/alura/test/service/ReajusteSalarialDesempenhoTest.java
+++ tddFuncionario/src/br/com/alura/test/service/ReajusteSalarialDesempenhoTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.main.modelo.Funcionario;
@@ -13,33 +14,30 @@ import br.com.alura.main.modelo.Desempenho;
 
 public class ReajusteSalarialDesempenhoTest {
 	
+	private ReajusteSalarialDesempenho reajusteDesempenho;
+	private Funcionario funcionario;
+	
+	@BeforeEach
+	public void inicializar() {
+		reajusteDesempenho = new ReajusteSalarialDesempenho();
+		funcionario = new Funcionario("Ruan", LocalDate.now(), new BigDecimal("1000"));
+	}
+	
 	@Test
 	public void reajusteDeveSerDeTresPorcentoSeODesempenhoForADesejar() {
-		
-		ReajusteSalarialDesempenho reajusteDesempenho = new ReajusteSalarialDesempenho();
-		Funcionario funcionario = new Funcionario("Ruan", LocalDate.now(), new BigDecimal("1000"));
 		reajusteDesempenho.reajustarSalario(funcionario, Desempenho.A_DESEJAR);
-		
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 	}
 	
 	@Test
 	public void reajusteDeveSerDeQuinzePorcentoSeODesempenhoForBom() {
-		
-		ReajusteSalarialDesempenho reajusteDesempenho = new ReajusteSalarialDesempenho();
-		Funcionario funcionario = new Funcionario("Ruan", LocalDate.now(), new BigDecimal("1000"));
 		reajusteDesempenho.reajustarSalario(funcionario, Desempenho.BOM);
-		
 		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
 	}
 	
 	@Test
 	public void reajusteDeveSerDeVintePorcentoSeODesempenhoForOtimo() {
-		
-		ReajusteSalarialDesempenho reajusteDesempenho = new ReajusteSalarialDesempenho();
-		Funcionario funcionario = new Funcionario("Ruan", LocalDate.now(), new BigDecimal("1000"));
 		reajusteDesempenho.reajustarSalario(funcionario, Desempenho.OTIMO);
-		
 		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
 	}
 
